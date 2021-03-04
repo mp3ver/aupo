4c17e03250b9d28d040aacc6a0f55accd35aa29f
mp3ver <mp3ver@mail.ru>
2021-03-04 18:07:18 +0500

    Second commit.

--- src/main/java/BankInfo.java
+++ src/main/java/BankInfo.java
@@ -15,13 +15,13 @@ public class BankInfo {
     private static String cardNumberRegex = "^\\d{4}-\\d{4}-\\d{4}-\\d{4}?";
 
     // тип операции
-    private static enum OperationTypes{
+    private static enum OperationTypes {
         DEPOSITION, // пополнение
         WITHDRAWAL // снятие
     }
 
     // статус операции
-    private static enum ConfirmationStatusTypes{
+    private static enum ConfirmationStatusTypes {
         CONFIRMED, // подтверждена
         REJECTED, // отклонена
         PROCESSING // обрабатывается
@@ -39,16 +39,16 @@ public class BankInfo {
         Scanner in = new Scanner(System.in);
         String inputSrt;
         int balance, menuNum = 0;
-        while(menuNum!=6){
+        while (menuNum != 6) {
             printMenu();
             inputSrt = in.nextLine();
-            while(!isNumber(inputSrt) || Integer.parseInt(inputSrt)>6 || Integer.parseInt(inputSrt)<1){
+            while (!isNumber(inputSrt) || Integer.parseInt(inputSrt) > 6 || Integer.parseInt(inputSrt) < 1) {
                 System.out.println("Неверный ввод");
                 System.out.print("Выберите пункт меню: ");
                 inputSrt = in.nextLine();
             }
             menuNum = Integer.parseInt(inputSrt);
-            switch (menuNum){
+            switch (menuNum) {
                 case 1:
                     System.out.print("Введите путь к целевой директории: ");
                     inputSrt = in.nextLine();
@@ -57,7 +57,7 @@ public class BankInfo {
                 case 2:
                     System.out.print("Введите ID клиента: ");
                     inputSrt = in.nextLine();
-                    while(!isNumber(inputSrt)){
+                    while (!isNumber(inputSrt)) {
                         System.out.println("Неверный ввод");
                         System.out.print("Введите ID клиента: ");
                         inputSrt = in.nextLine();
@@ -68,7 +68,7 @@ public class BankInfo {
                 case 3:
                     System.out.print("Введите номер карты: ");
                     inputSrt = in.nextLine();
-                    while(!isCardNumber(inputSrt)){
+                    while (!isCardNumber(inputSrt)) {
                         System.out.println("Неверный ввод");
                         System.out.print("Введите номер карты: ");
                         inputSrt = in.nextLine();
@@ -89,13 +89,12 @@ public class BankInfo {
     }
 
     // вывод меню
-    private static void printMenu(){
+    private static void printMenu() {
         System.out.println();
-        if(targetDirectoryPath==null){
+        if (targetDirectoryPath == null) {
             System.out.println("Целевая директория: не задано");
-        }
-        else{
-            System.out.println(String.format("Целевая директория: %s",targetDirectoryPath));
+        } else {
+            System.out.println(String.format("Целевая директория: %s", targetDirectoryPath));
         }
         System.out.println("1. Задать целевую директорию");
         System.out.println("2. Баланс операций по указанному клиенту");
@@ -108,73 +107,70 @@ public class BankInfo {
 
     // Метод для получения списка csv-файлов в целевой директории
     private static void getListOfCsv() throws FileNotFoundException, NullPointerException {
-            if(targetDirectoryPath.equals(null)){
-                throw new NullPointerException();
-            }
-            File targetDirectory = new File(targetDirectoryPath);
-            if(!targetDirectory.exists()){
-                throw new FileNotFoundException(String.format("Директории \"%s\" не существует!",targetDirectoryPath));
-            }
-            listOfCsv.clear();
-            for(File fileSystemObject : targetDirectory.listFiles()){
-                if(fileSystemObject.isFile()){
-                    // Проверяем имя файла на соответствие шаблону "ДД-ММ-ГГГГ.csv"
-                    Pattern pattern = Pattern.compile(fileNameRegex);
-                    Matcher matcher = pattern.matcher(fileSystemObject.getName());
-                    if(matcher.matches()){
-                        listOfCsv.add(fileSystemObject.getAbsolutePath());
-                    }
+        if (targetDirectoryPath.equals(null)) {
+            throw new NullPointerException();
+        }
+        File targetDirectory = new File(targetDirectoryPath);
+        if (!targetDirectory.exists()) {
+            throw new FileNotFoundException(String.format("Директории \"%s\" не существует!", targetDirectoryPath));
+        }
+        listOfCsv.clear();
+        for (File fileSystemObject : targetDirectory.listFiles()) {
+            if (fileSystemObject.isFile()) {
+                // Проверяем имя файла на соответствие шаблону "ДД-ММ-ГГГГ.csv"
+                Pattern pattern = Pattern.compile(fileNameRegex);
+                Matcher matcher = pattern.matcher(fileSystemObject.getName());
+                if (matcher.matches()) {
+                    listOfCsv.add(fileSystemObject.getAbsolutePath());
                 }
             }
+        }
     }
 
     // функция проверяет, представляет ли из себя строка число
-    private static boolean isNumber(String str){
-        if(str == null){
+    private static boolean isNumber(String str) {
+        if (str == null) {
             return false;
         }
-        try{
+        try {
             Integer.parseInt(str);
-        }
-        catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             return false;
         }
         return true;
     }
 
     // функция проверяет, представляет ли из себя строка элемент перечисления
-    private static <E extends Enum<E>> boolean isValidEnumValue(String str, Class<E> enumClass){
-        if(str == null){
+    private static <E extends Enum<E>> boolean isValidEnumValue(String str, Class<E> enumClass) {
+        if (str == null) {
             return false;
         }
-        try{
+        try {
             Enum.valueOf(enumClass, str);
-        }
-        catch(IllegalArgumentException ex){
+        } catch (IllegalArgumentException ex) {
             return false;
         }
         return true;
     }
 
     // функция проверяет, представляет ли из себя строка номер банковской карты
-    private static boolean isCardNumber(String str){
-        if(str == null){
+    private static boolean isCardNumber(String str) {
+        if (str == null) {
             return false;
         }
         Pattern pattern = Pattern.compile(cardNumberRegex);
         Matcher matcher;
         matcher = pattern.matcher(str);
-        if (!matcher.matches()){
+        if (!matcher.matches()) {
             return false;
-        }
-        else{
+        } else {
             return true;
         }
     }
 
     // функция проверяет строку из csv-файла на соответствие формату данных
     // если строка содержит ошибку, то возвращается её описание, иначе - null
-    private static String checkRow(String[] row, int rowNumber, String fileName){
+    private static String checkRow(String[] row, int rowNumber, String fileName) {
         String checkResult;
         if (row.length < 7) {
             checkResult = String.format("%s : в строке %d содержатся данные в неверном формате", fileName, rowNumber);
@@ -188,7 +184,7 @@ public class BankInfo {
             checkResult = String.format("%s : в строке %d содержится признак подтверждения операции в неверном формате", fileName, rowNumber);
             return checkResult;
         }
-        if(!isNumber(row[5])){
+        if (!isNumber(row[5])) {
             checkResult = String.format("%s : в строке %d содержится сумма операции в неверном формате", fileName, rowNumber);
             return checkResult;
         }
@@ -209,7 +205,7 @@ public class BankInfo {
         int rowNumber = 0;
         int balance = 0;
         int idFromFile, operationSum;
-        int  clientId;
+        int clientId;
         String cardNumber, numberFromFile, checkResult;
         OperationTypes operationType;
         ConfirmationStatusTypes confirmationStatus;
@@ -223,15 +219,14 @@ public class BankInfo {
             while ((row = reader.readNext()) != null) {
                 rowNumber += 1;
                 checkResult = checkRow(row, rowNumber, csvFile.getName());
-                if(checkResult != null){
+                if (checkResult != null) {
                     System.out.println(checkResult);
                     continue;
-                }
-                else{
+                } else {
                     operationType = OperationTypes.valueOf(row[4]);
                     confirmationStatus = ConfirmationStatusTypes.valueOf(row[6]);
                     operationSum = Integer.parseInt(row[5]);
-                    if(operationType == OperationTypes.WITHDRAWAL){
+                    if (operationType == OperationTypes.WITHDRAWAL) {
                         operationSum *= -1; // если тип операции - "снятие", то делаем сумму операции отрицательной
                     }
                     idFromFile = Integer.parseInt(row[0]);
@@ -263,38 +258,33 @@ public class BankInfo {
                 }
 
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
             return 0;
-        }
-        catch(IOException e){
+        } catch (IOException e) {
+            System.out.println("Неверный формат csv");
+            return 0;
+        } catch (CsvValidationException e) {
             System.out.println("Неверный формат csv");
             return 0;
         }
-        catch(CsvValidationException e){
-            System.out.println("Неверный формат csv");
-            return 0;
-        }
-    return balance;
+        return balance;
     }
 
     // функция подсчёта баланса операций по данным из всех файлов;
     // переменная identifier может содержать ID клиента, номер карты или быть пустой в зависимости от значения identifierType
-    public static int balanceByAllFiles(String identifier, IdentifierTypes identifierType){
-        try{
+    public static int balanceByAllFiles(String identifier, IdentifierTypes identifierType) {
+        try {
             getListOfCsv();
             int balance = 0;
-            for(String csvFile : listOfCsv){
+            for (String csvFile : listOfCsv) {
                 balance += balanceBySingleFile(identifier, identifierType, csvFile);
             }
             return balance;
-        }
-        catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
             return 0;
-        }
-        catch(NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("Целевая директория не задана");
             return 0;
         }
